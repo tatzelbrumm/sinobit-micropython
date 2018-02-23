3b680c962d6611e71366e58f69381341e88f11f0
Tony DiCola <tony@tonydicola.com>
2018-02-23 01:07:34 -0800

    Re-enable LTO but disable warning in sinobit board.

--- ports/nrf/boards/sinobit/mpconfigboard.mk
+++ ports/nrf/boards/sinobit/mpconfigboard.mk
@@ -3,3 +3,7 @@ MCU_VARIANT = nrf51
 MCU_SUB_VARIANT = nrf51822
 LD_FILE = boards/nrf51x22_256k_16k.ld
 FLASHER = pyocd
+
+# Disable lto type mismatch warning because of this bug in GCC:
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81440
+CFLAGS += -Wno-lto-type-mismatch
