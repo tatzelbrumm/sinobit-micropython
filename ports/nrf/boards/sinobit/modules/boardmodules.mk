3ff9c614857e618110d9c61c2021736110cb190f
Tony DiCola <tony@tonydicola.com>
2018-02-23 00:26:51 -0800

    Fix makefile building board modules, disable LTO (broken in GCC), add sinobit board.

---
+++ ports/nrf/boards/sinobit/modules/boardmodules.mk
@@ -0,0 +1,15 @@
+BOARD_SINOBIT_DIR = boards/sinobit/modules
+
+INC += -I./$(BOARD_SINOBIT_DIR)
+CFLAGS += -DBOARD_SPECIFIC_MODULES
+
+SRC_BOARD_MODULES = $(addprefix $(BOARD_SINOBIT_DIR)/,\
+                      microbitdisplay.c \
+                      microbitimage.c \
+                      iters.c \
+                      microbitconstimage.c \
+                      microbitconstimagetuples.c \
+                      modmicrobit.c \
+                      )
+
+OBJ += $(addprefix $(BUILD)/, $(SRC_BOARD_MODULES:.c=.o))
